1cc6a828fc02671805dbf52d984d0732aefd55c5
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-15 15:08:55 -0500

    Created the task 4 and 5, return json files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,10 +10,10 @@ from models.user import User
 from models.amenity import Amenity
 from models.review import Review
 
-
 classes = {"amenities": Amenity, "cities": City,
            "places": Place, "reviews": Review,
            "States": State, "users": User}
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
