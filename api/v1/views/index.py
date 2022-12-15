977a429c2b231ed129c2bcfd0d3f51a039ff3ac2
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-15 13:09:14 -0500

    Created the task 4 and 5, return json files

---
+++ api/v1/views/index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module created the routes"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.user import User
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review,
+           "States": State, "users": User}
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status ():
+    """This route return a json"""
+    return jsonify({"statis": "OK"})
+
+@app_views.route('/api/v1/stats')
+def stats():
+    """This method return the numbers of the objects"""
+    number_objects = {}
+    for key, value in classes:
+        number_objects[key] = storage.count(value)
+    return number_objects
