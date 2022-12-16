c7e4309bf5bc1f9e5e7efaa6fcbba1de62ddeeb0
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-16 16:14:14 -0500

    update

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -12,3 +12,48 @@ list_met = ['GET', 'DELETE', 'POST', 'PUT']
 @app_views.route('/users', methods=list_met, strict_slashes=False)
 @app_views.route('/users/<user_id>', methods=list_met, strict_slashes=False)
 def states(user_id=None):
+    """
+    """
+    if request.method == 'GET':
+        if state_id:
+            obj = storage.get(State, state_id)
+            if obj:
+                return jsonify(obj.to_dict())
+            return make_response(jsonify({'error': 'Not found'}), 404)
+        state_objs = [state.to_dict() for state in storage.all(State).values()]
+        return jsonify(state_objs)
+
+    if request.method == 'DELETE':
+        if user_id:
+            obj = storage.get(User, user_id)
+            if obj:
+                storage.delete(obj)
+                storage.save()
+                return make_response(jsonify({}), 200)
+            return make_response(jsonify({'error': 'Not found'}), 404)
+
+    if request.method == 'POST':
+        content = request.get_json()
+        if content:
+            if "name" not in content:
+                return make_response(jsonify("error"": Missing name"), 400)
+            else:
+                new = User(**content)
+                new.save()
+                return make_response(jsonify(new.to_dict()), 201)
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+    if request.method == 'PUT':
+        if user_id:
+            content = request.get_json()
+            if not content:
+                return make_response(jsonify({"error": "Not a JSON"}), 400)
+            user = storage.get(User, user_id)
+            if not user:
+                return make_response(jsonify({"error": "Not found"}), 404)
+            for key, value in content.items():
+                if key != "id" and key != "created_at" and key != "updated_at":
+                    setattr(user, key, value)
+                    user.save()
+                    return make_response(jsonify(user.to_dict()), 200)
+        return make_response(jsonify({"error": "Not found"}), 404)
