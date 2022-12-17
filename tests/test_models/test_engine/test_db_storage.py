aeebfef269c8597a76b9f0250cfbe9447a32afb1
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-16 23:19:06 -0500

    update

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -100,4 +100,4 @@ class TestFileStorage(unittest.TestCase):
         state_1 = State()
         state_2 = State()
         state_3 = State()
-        self.assertEqual(storage.count(State), 1)
+        self.assertEqual(storage.count(State), len(storage.all(State))
